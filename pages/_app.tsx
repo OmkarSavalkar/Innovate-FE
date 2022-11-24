9f77d8927552226e450d7e3971c1634daab547cc
sayali1910 <sayalikanase1910@gmail.com>
2022-11-24 14:42:32 +0530

    Add redux store and hooks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RouteGuard } from "../utils/routerGaurd";
-
+import { store } from "../side-effects/store";
+import { Provider } from "react-redux";
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <RouteGuard>
-      <Component {...pageProps} />
-    </RouteGuard>
+    <Provider store={store}>
+      <RouteGuard>
+        <Component {...pageProps} />
+      </RouteGuard>
+    </Provider>
   );
   // <RouteGuard>{getLayout(<Component {...pageProps} />)}</RouteGuard>
 }
