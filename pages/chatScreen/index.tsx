5a85e52baf413f7b7957a767b4efc766418d0f30
sayali1910 <sayalikanase1910@gmail.com>
2022-12-12 13:39:38 +0530

    change discuss forum to Q&A

--- pages/chatScreen/index.tsx
+++ pages/chatScreen/index.tsx
@@ -5,8 +5,8 @@ const ChatScreen = () => {
   return (
     <LayoutComponent
       secondaryLayout={true}
-      secondaryLayoutTitle={"Welcome to discuss forum"}
-      sideBarList={[{ sideBarName: "Discuss Forum", secondaryLayout: true }]}
+      secondaryLayoutTitle={"Welcome to Q&A forum"}
+      sideBarList={[{ sideBarName: "Q&A Forum", secondaryLayout: true }]}
       sideBarNavigationList={[{ page: "Dashboard", pageUrl: "/dashboard" }]}
       main={[<ChatComponent key={0} />]}
     />
